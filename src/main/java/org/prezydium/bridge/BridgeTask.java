2c937069fbf1488c3bfc2ea0fead41a8b3de8433
Krzysztof Syska <prezydium834@gmail.com>
2019-06-21 08:56:38 +0200

    Sixth exercise - bridge pattern

---
+++ src/main/java/org/prezydium/bridge/BridgeTask.java
@@ -0,0 +1,66 @@
+package org.prezydium.bridge;
+
+public class BridgeTask {
+}
+
+abstract class Shape {
+
+    private final Renderer renderer;
+
+    public Shape(Renderer renderer) {
+        this.renderer = renderer;
+    }
+
+    public abstract String getName();
+
+    @Override
+    public String toString() {
+        return "Drawing " + getName() + " as " + renderer.whatToRenderAs();
+    }
+}
+
+interface Renderer {
+
+    public String whatToRenderAs();
+}
+
+class Triangle extends Shape {
+
+    public Triangle(Renderer renderer) {
+        super(renderer);
+    }
+
+    @Override
+    public String getName() {
+        return "Triangle";
+    }
+}
+
+class Square extends Shape {
+
+    public Square(Renderer renderer) {
+        super(renderer);
+    }
+
+    @Override
+    public String getName() {
+        return "Square";
+    }
+}
+
+class VectorRenderer implements Renderer{
+
+    @Override
+    public String whatToRenderAs() {
+        return "lines";
+    }
+}
+
+class RasterRenderer implements Renderer{
+
+    @Override
+    public String whatToRenderAs() {
+        return "pixels";
+    }
+}
+
